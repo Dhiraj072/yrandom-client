3c9161f0c34e4e123cb382388373c67d048dda32
Dhiraj <dhirajbhatt072@gmail.com>
2018-12-31 02:35:47 +0800

    Remove logs

--- src/services/services.js
+++ src/services/services.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export function getRandomVideo() {
-    console.log("Original!");
     return axios.get('http://yrandom.ap-southeast-1.elasticbeanstalk.com/yrandom/video/random')
     .then((resp) => {
         return resp;
